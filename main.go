3892587f0d13078ed76f03c220bc3c015dc0ba37
Shuho Ohwada <hanzou6326@gmail.com>
2020-04-11 17:42:33 +0900

    refactor argument parser

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/docopt/docopt.go"
+	"log"
 	"os"
 )
 
@@ -10,23 +11,21 @@ const version = "0.1.0-alpha"
 const usage = `Last-Split To Global Alignment on graph structure
 
 Usage:
-  ls2ga [--min-aln-size=<N>] [--gfa=<FILE>] [--json=<FILE>] <fastaFile> <mafFile>
-  ls2ga --version | -V
-  ls2ga --help | -h
+  ls2ga [--node-max N] [--min-aln-size N] [--gfa FILE] [--json FILE] <fastaFile> <mafFile>
 
 Options:
-  -g --gfa=<FILE>        output GFA file [default: graph.gfa]
-  -j --json=<FILE>       output alignment file [default: aln.json]
-  -M --node-max=<N>      maximum node sequence size [default: 1000]
-  -m --min-aln-size=<N>  minimum alignment block size [default: 1000]
-  -V --version           show version
-  -h --help              show this document
+  -g --gfa FILE        output GFA file [default: graph.gfa]
+  -j --json FILE       output alignment file [default: aln.json]
+  -M --node-max N      maximum node sequence size [default: 1000]
+  -m --min-aln-size N  minimum alignment block size [default: 1000]
+  -V --version         show version
+  -h --help            show this document
 `
 
 // Command line arguments
 type Args struct {
 	MinAlnSize  int    `docopt:"--min-aln-size"`
-	NodeMaxSize int    `docopt:"node-max"`
+	NodeMaxSize int    `docopt:"--node-max"`
 	GfaPath     string `docopt:"--gfa"`
 	JsonPath    string `docopt:"--json"`
 	FastaPath   string `docopt:"<fastaFile>"`
@@ -35,19 +34,20 @@ type Args struct {
 
 // Parse command line arguments
 func parseArgument(argv []string) Args {
-	parser := &docopt.Parser{}
+	if len(argv) == 0 {
+		fmt.Print(usage)
+		os.Exit(0)
+	}
 
-	opts, err := parser.ParseArgs(usage, argv, version)
+	opts, err := (&docopt.Parser{}).ParseArgs(usage, argv, version)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s", err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 
 	var args Args
 	err = opts.Bind(&args)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s", err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 	return args
 }
