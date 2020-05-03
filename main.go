3ee4daadc5adcdc44ad1ddcdf857c6d8d2e3781f
Shuho Ohwada <hanzou6326@gmail.com>
2020-05-03 19:06:27 +0900

    add query.fa

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ const version = "0.1.0-alpha"
 const usage = `Last-Split To Global Alignment on graph structure
 
 Usage:
-  ls2ga [--node-max N] [--min-aln-size N] [--gfa FILE] [--json FILE] <fastaFile> <mafFile>
+  ls2ga [--node-max N] [--min-aln-size N] [--gfa FILE] [--json FILE] <target.fa> <query.fa> <aln.maf>
 
 Options:
   -g --gfa FILE        output GFA file [default: graph.gfa]
@@ -19,17 +19,18 @@ Options:
   -M --node-max N      maximum node sequence size [default: 1000]
   -m --min-aln-size N  minimum alignment block size [default: 1000]
   -V --version         show version
-  -h --help            show this document
+  -h --help            show this help message
 `
 
 // Command line arguments
 type Args struct {
-	MinAlnSize  int    `docopt:"--min-aln-size"`
-	NodeMaxSize int    `docopt:"--node-max"`
-	GfaPath     string `docopt:"--gfa"`
-	JsonPath    string `docopt:"--json"`
-	FastaPath   string `docopt:"<fastaFile>"`
-	MafPath     string `docopt:"<mafFile>"`
+	MinAlnSize      int    `docopt:"--min-aln-size"`
+	NodeMaxSize     int    `docopt:"--node-max"`
+	GfaPath         string `docopt:"--gfa"`
+	JsonPath        string `docopt:"--json"`
+	TargetFastaPath string `docopt:"<target.fa>"`
+	QueryFastaPath  string `docopt:"<query.fa>"`
+	MafPath         string `docopt:"<aln.maf>"`
 }
 
 // Parse command line arguments
